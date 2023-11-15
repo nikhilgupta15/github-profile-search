81211510169ab9787ea23db1776184d9f5b4d8a6
Nikhil <nikhilgupta150498@gmail.com>
2023-11-15 18:12:41 +0530

    Added search image

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { Repos, User } from "@/types";
 import UserDetails from "./_components/UserDetails";
+import SearchImage from "../public/SearchImage.jpg";
+import Image from "next/image";
 
 export default function Home() {
   const [searchedUser, setSearchedUser] = useState<User>(null);
@@ -47,10 +49,17 @@ export default function Home() {
           />
         )}
         {!searchedUser && (
-          <div className="flex justify-center items-center h-[200px]">
+          <div className="flex flex-col justify-center items-center h-fit mt-6">
             <p className="text-[#abb4c2] text-4xl text-center">
-              Search for users to view their details!!
+              Search for users to view their Github Profiles!!
             </p>
+            <Image
+              src={SearchImage}
+              alt="search-image"
+              className="object-contain mt-6 mb-6 "
+              height={150}
+              width={150}
+            />
           </div>
         )}
       </div>
